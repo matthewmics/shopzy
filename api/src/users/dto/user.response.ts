018ae1b8a0a52dc703a86663ed874c743e626407
Matthew Miclat <matthewmics77@gmail.com>
2025-09-25 11:10:55 +0800

    Use response dto

--- api/src/users/dto/user.response.ts
+++ api/src/users/dto/user.response.ts
@@ -1,5 +1,11 @@
+import { Expose } from "class-transformer";
+
 export class UserResponse {
+
+  @Expose()
   readonly id: number;
+
+  @Expose()
   readonly email: string;
 
   constructor(partial: Partial<UserResponse> = {}) {
--- api/src/users/users.service.ts
+++ api/src/users/users.service.ts
@@ -3,21 +3,23 @@ import { CreateUserRequest } from './dto/create-user.request';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { User } from 'generated/prisma';
 import * as bcrypt from 'bcrypt';
+import { UserResponse } from './dto/user.response';
+import { plainToInstance } from 'class-transformer';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly prismaService: PrismaService) {}
-  async createUser(data: CreateUserRequest) {
+  async createUser(data: CreateUserRequest): Promise<UserResponse> {
     try {
-      return await this.prismaService.user.create({
+      const user = await this.prismaService.user.create({
         data: {
           ...data,
           password: await bcrypt.hash(data.password, 10),
         },
-        select: {
-          id: true,
-          email: true,
-        },
+      });
+
+      return plainToInstance(UserResponse, user, {
+        excludeExtraneousValues: true,
       });
     } catch (error) {
       if (error.code === 'P2002') {
