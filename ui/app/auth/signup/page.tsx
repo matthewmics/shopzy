cd7a2b1bbbd2cdc8c7b843bf127cd29af1c64249
Matthew Miclat <matthewmics77@gmail.com>
2025-09-26 10:56:18 +0800

    Create user

--- ui/app/auth/signup/page.tsx
+++ ui/app/auth/signup/page.tsx
@@ -1,21 +1,42 @@
-import React from "react";
+"use client";
+
+import React, { useActionState } from "react";
 import { Button, Link, Stack, TextField } from "@mui/material";
 import NextLink from "next/link";
+import createUser from "./create-user";
 
 const page = () => {
+  const [state, formAction] = useActionState(createUser, {
+    error: "",
+  });
+
   return (
-    <Stack spacing={2} className="w-full max-w-xs">
-      <TextField label={"Email"} variant={"outlined"} type="email"></TextField>
-      <TextField
-        label={"Password"}
-        variant={"outlined"}
-        type="password"
-      ></TextField>
-      <Button variant={"contained"}>Signup</Button>
-      <Link component={NextLink} href="/auth/login" className="self-center">
-        Login
-      </Link>
-    </Stack>
+    <form action={formAction} className="w-full max-w-xs">
+      <Stack spacing={2}>
+        <TextField
+          name="email"
+          label={"Email"}
+          variant={"outlined"}
+          type="email"
+          helperText={state.error}
+          error={!!state.error}
+        ></TextField>
+        <TextField
+          name="password"
+          label={"Password"}
+          variant={"outlined"}
+          type="password"
+          helperText={state.error}
+          error={!!state.error}
+        ></TextField>
+        <Button type="submit" variant={"contained"}>
+          Signup
+        </Button>
+        <Link component={NextLink} href="/auth/login" className="self-center">
+          Login
+        </Link>
+      </Stack>
+    </form>
   );
 };
 
