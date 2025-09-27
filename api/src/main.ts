49aad7eaf875288b4bfb713f5547edf30944c5a5
= <=>
2025-09-27 18:24:28 +0800

    Fix CI

--- api/src/main.ts
+++ api/src/main.ts
@@ -4,6 +4,7 @@ import type { AddressInfo } from 'net';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Logger } from 'nestjs-pino';
+import { Address } from 'cluster';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
